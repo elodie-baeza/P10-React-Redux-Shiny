77bcee8e51ca15f7620a271dbf555dd65438c605
elodie baeza <elodie.baeza@gmail.com>
2021-11-04 23:54:44 +0100

    results - creatSlice

--- src/features/results.js
+++ src/features/results.js
@@ -1,4 +1,4 @@
-import { createAction, createReducer } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit"
 import { selectResults } from "../utils/selectors"
 
 //initialState
@@ -9,22 +9,58 @@ const initialState = {
     params: null
 }
 
-// action creator
-const resultsFetching = createAction('results/fetching', (params) => {
-    return {
-        payload: { params }
-    }
-})
-
-const resultsResolved = createAction('results/resolved', (params, data) => {
-    return {
-        payload: { params, data }
-    }
-})
-
-const resultsRejected = createAction('results/rejected', (params, error) => {
-    return {
-        payload: { params, error }
+const {actions, reducer} = createSlice({
+    name: 'results',
+    initialState,
+    reducers: {
+        fetching: {
+            prepare: (params) => ({
+                    payload: { params }
+            }),
+            reducer: (draft, action) => {
+                const params = action.payload.params
+                if (draft.status === 'void') {
+                    draft.status = 'pending'
+                    draft.params = params
+                    return
+                }
+                draft.status = 'updating'
+                draft.params = params
+            }
+        },
+        resolved: {
+            prepare: (params, data) => ({
+                    payload: { params, data }
+            }),
+            reducer: (draft, action) => {
+                if (draft.params !== action.payload.params) {
+                    return
+                }
+                if (draft.status === 'pending' || draft.status === 'updating') {
+                    draft.data = action.payload
+                    draft.status = 'resolved'
+                    return
+                }
+                return    
+            }
+        },
+        rejected: {
+            prepare: (params, error) => ({
+                payload: { params, error }
+        }),
+            reducer: (draft, action) => {
+                if (draft.params !== action.payload.params){
+                    return
+                }
+                if (draft.status === 'pending' || draft.status === 'updating') {
+                    draft.error = action.payload
+                    draft.data = null
+                    draft.status = 'rejected'
+                    return
+                }
+                return    
+            }
+        }
     }
 })
 
@@ -33,54 +69,18 @@ export function fetchOrUpdateResults(params){
     return async (dispatch, getState) => {
         const results = selectResults(getState())
         if (results.status === 'void' || results.params !== params ){
-            dispatch(resultsFetching(params))
+            dispatch(actions.fetching(params))
         }
         try {
             const response = await fetch(
               `http://localhost:8000/results?${params}`
             )
             const data = await response.json()
-            dispatch(resultsResolved(params, data))
+            dispatch(actions.resolved(params, data))
         } catch (error) {
-            dispatch(resultsRejected(params, error))
+            dispatch(actions.rejected(params, error))
         }
     }
 }
 
-// reducer
-export default createReducer(initialState, builder => 
-    builder
-        .addCase(resultsFetching, (draft, action) => {
-            const params = action.payload.params
-            if (draft.status === 'void') {
-                draft.status = 'pending'
-                draft.params = params
-                return
-            }
-            draft.status = 'updating'
-            draft.params = params
-        })
-        .addCase(resultsResolved, (draft, action) => {
-            if (draft.params !== action.payload.params) {
-                return
-            }
-            if (draft.status === 'pending' || draft.status === 'updating') {
-                draft.data = action.payload
-                draft.status = 'resolved'
-                return
-            }
-            return    
-        })
-        .addCase(resultsRejected, (draft, action) => {
-            if (draft.params !== action.payload.params){
-                return
-            }
-            if (draft.status === 'pending' || draft.status === 'updating') {
-                draft.error = action.payload
-                draft.data = null
-                draft.status = 'rejected'
-                return
-            }
-            return    
-        })
-)
+export default reducer
