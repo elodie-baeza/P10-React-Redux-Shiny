761944799c854069c130c73b648e9650cdd0f403
elodie baeza <elodie.baeza@gmail.com>
2021-11-04 15:12:42 +0100

    Redux Toolkit_configureStore

--- src/utils/store.js
+++ src/utils/store.js
@@ -1,31 +1,16 @@
-import { combineReducers, createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import freelanceReducer from "../features/freelance";
 import freelancesReducer from "../features/freelances";
 import surveyReducer from "../features/survey";
 import themeReducer from "../features/theme";
 
-// on utilise combineReducer pour faire
-// fonctionner plusieurs reducers ensemble
-const reducer = combineReducers({
-    // le themeReducer est responsable de la propriété `theme` du state
-    theme: themeReducer,
-    freelances: freelancesReducer,
-    survey: surveyReducer,
-    freelance: freelanceReducer,
+const store = configureStore({
+    reducer: {
+        theme: themeReducer,
+        freelances: freelancesReducer,
+        survey: surveyReducer,
+        freelance: freelanceReducer,
+    }
 })
-
-// Pour connecter les Redux Devtools on utilise
-// un fonction disponible sur l'objet window
-// Si cette fonction existe on l'exécute.
-const reduxDevtools = 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-
-// on utilise le reducer créer avec combineReducers
-// pour initialiser le store
-// Pas besoin de passer de state initial
-// car chaque reducer à son propre state initial
-
-// on utilise le résultat de cette fonction en parametre de createStore
-const store = createStore(reducer, reduxDevtools)
 
 export default store
